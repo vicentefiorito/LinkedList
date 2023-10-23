f12b591e84222f63448b8290b470dd7cc133f344
vicentefiorito <vicentefiorito@gmail.com>
2023-10-23 18:28:21 -0400

    impelemented append() method to add to the tail of the list

--- linkedlist.js
+++ linkedlist.js
@@ -5,8 +5,30 @@ const Node = (value,nextNode) => {
     return {value,nextNode}
 }
 
-// testing
-const test1 = Node(5,7)
+// Linked List function implementation
+const LinkedList = (list) => {
+    // append module
+    const append = (value) => {
+        // if the value is already in the list
+        if(list.includes(value)) {
+            console.log('Value you are trying to append is on the list already')
+            return
+        } else {
+            list.push(value)
+        }
+    }
 
-console.log('Node Value --> ' + test1.value)
-console.log('Node next pointer --> ' + test1.nextNode)
+    return {append}
+}
+
+// testing
+const list1 = [3,5,6]
+
+const linkedList1 = LinkedList(list1)
+
+const node1 = Node(5,7)
+
+linkedList1.append(node1.value)
+
+// console.log('Node Value --> ' + node1.value)
+// console.log('Node next pointer --> ' + node1.nextNode)
